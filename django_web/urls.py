93a4eac14a08d96cc9bff98e486558bb9455ec91
yuaga <yg2017yg@163.com>
2019-06-03 21:11:14 +0800

    edit homepage url

--- django_web/urls.py
+++ django_web/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import RedirectView
+from apps.blog import views
 
 urlpatterns = [
+    path('', views.index),
     path('blog/', include('apps.blog.urls')),
     path('news/', include('apps.news.urls')),
     path('cms/', include('apps.cms.urls')),
